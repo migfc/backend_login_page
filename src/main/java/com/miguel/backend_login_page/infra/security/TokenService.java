ec952b5696f2bc7840471793de8226f4dde5d7df
Miguel de Freitas Comibra <miguel.freitas.coimbra18@gmail.com>
2025-11-19 20:21:48 -0300

    criando a classe TokenService

---
+++ src/main/java/com/miguel/backend_login_page/infra/security/TokenService.java
@@ -0,0 +1,19 @@
+package com.miguel.backend_login_page.infra.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.miguel.backend_login_page.domain.user.User;
+
+@Service
+public class TokenService {
+
+}
