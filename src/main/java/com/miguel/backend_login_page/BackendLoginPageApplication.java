56a5caf7c5ac31cf9284a7929ce5714f4781147e
Miguel de Freitas Comibra <miguel.freitas.coimbra18@gmail.com>
2025-11-19 19:29:23 -0300

    commit inicial

---
+++ src/main/java/com/miguel/backend_login_page/BackendLoginPageApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.backend_login_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLoginPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLoginPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/backend_login_page/BackendLoginPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.backend_login_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLoginPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
