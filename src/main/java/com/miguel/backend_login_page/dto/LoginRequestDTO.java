b4c79064cd93cec94450a1b80500dbace8258a6a
Miguel Silva <miguel@example.com>
2025-11-29 18:45:35 -0300

    chore: finalize remaining source files and fix minor issues

---
+++ src/main/java/com/miguel/backend_login_page/dto/LoginRequestDTO.java
@@ -0,0 +1,5 @@
+package com.miguel.backend_login_page.dto;
+
+public record LoginRequestDTO(String email, String password) {
+
+}
---
+++ src/main/java/com/miguel/backend_login_page/dto/ResponseDTO.java
@@ -0,0 +1,5 @@
+package com.miguel.backend_login_page.dto;
+
+public record ResponseDTO(String name, String token) {
+
+}
